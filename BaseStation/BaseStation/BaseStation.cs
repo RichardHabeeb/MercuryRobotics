c42905159dcc181be8db0c458ba908bb82405a04
Richard Habeeb <richard.habeeb@gmail.com>
2014-12-11 20:14:52 -0600

    initial commit

---
+++ BaseStation/BaseStation/BaseStation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaseStation
+{
+    public partial class BaseStation : Form
+    {
+        public BaseStation()
+        {
+            InitializeComponent();
+        }
+
+        private void BaseStation_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
