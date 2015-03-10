36790f5867751bc5efe466db5e6ed04266efdd0c
Richard Habeeb <richard.habeeb@gmail.com>
2015-03-09 22:27:14 -0500

    changes to the stepper motor classes, this is pretty stable with the basic acceleration program

--- stepper_motor/stepper_motor.h
+++ stepper_motor/stepper_motor.h
@@ -19,7 +19,7 @@
 /*-------------------------------------------------------------------------------------------------
 *                                       Literal Definitions
 *------------------------------------------------------------------------------------------------*/
-#define MINIMUM_STEP_PERIOD_US         (10000)
+#define MAX_STEP_PERIOD_US             (10000) /* period here is refering to 1/2 T (we count a toggle as a period) */
 #define WHEEL_RADIUS                   (.03f)
 #define STEPS_PER_REVOLUTION           (200.0f)
 #define TICKS_PER_STEP                 (4.0f) 
@@ -27,7 +27,7 @@
 #define WHEEL_CIRCUMFERENCE            (2.0f * PI * WHEEL_RADIUS)
 #define MIN_VELOCITY                   (0.0f)  /* Forward velocity (not angular). */
 #define MAX_VELOCITY                   (0.2f)  /* Forward velocity (not angular). */
-#define ACCELERATION_TIME              (3.0f)
+#define ACCELERATION                   (0.1f / 3.0f) /* arbitrary TODO */
 
 /*-------------------------------------------------------------------------------------------------
 *                                           Constants
@@ -37,7 +37,7 @@
 *                                             Types
 *------------------------------------------------------------------------------------------------*/
 /* TODO */
-typedef void (*step_fuction_ptr)(void);
+typedef void (*step_fuction_t)(void);
 
 /* TODO */
 typedef uint8_t motor_direction_t;
@@ -67,16 +67,16 @@ enum
 class stepper_motor
 {
   public:  /* Methods */
-    stepper_motor(uint8_t pwm_pin, uint8_t step_pin, uint8_t direction_pin, uint8_t micro_select_1_pin, uint8_t micro_select_2_pin, uint8_t micro_select_3_pin, timer_select_t timer_select);
+    stepper_motor(uint8_t pwm_pin, uint8_t step_pin, uint8_t direction_pin, uint8_t micro_select_1_pin, uint8_t micro_select_2_pin, uint8_t micro_select_3_pin, timer_select_t timer_select, step_fuction_t step_function_wrapper);
     ~stepper_motor(void);
+    void start(void);
     void stop(void);
-    void set_target_velocity(float v) { velocity = v; }
+    void set_target_velocity(float v) { target_velocity = v; }
     void set_motor_rotation_direction(motor_direction_t d) { rotation_direction = d; }
+    void step(void);
     
   private: /* Methods */
-    void step(void);
-    float get_acceleration(void) const { return (target_velocity - velocity) / ACCELERATION_TIME; }
-    unsigned long get_step_period(void) const { return min(MINIMUM_STEP_PERIOD_US, WHEEL_CIRCUMFERENCE / (TICKS_PER_REVOLUTION * (velocity)) * 100000); }  /* Forward velocity. Maxes out at MINIMUM_STEP_PERIOD_US microseconds */
+    unsigned long get_step_period(void) const { return min(MAX_STEP_PERIOD_US, (100000.0f * WHEEL_CIRCUMFERENCE / (TICKS_PER_REVOLUTION * velocity))); }  /* Forward velocity. Maxes out at MINIMUM_STEP_PERIOD_US microseconds */
     
   public:  /* Fields */
   
@@ -90,6 +90,7 @@ class stepper_motor
     uint8_t motor_signal_state;
     volatile float velocity;
     volatile float target_velocity;
+    step_fuction_t step_function_wrapper;
     motor_direction_t rotation_direction;
     timer_select_t timer_select;
 };
