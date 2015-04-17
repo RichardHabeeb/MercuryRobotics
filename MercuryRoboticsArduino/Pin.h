4e952e80198ef15cd03e13ad75c9825204df5a5c
Richard Habeeb <richard.habeeb@gmail.com>
2015-04-16 21:03:54 -0500

    Updated pin defs.

--- MercuryRoboticsArduino/Pin.h
+++ MercuryRoboticsArduino/Pin.h
@@ -19,26 +19,32 @@
 /*-------------------------------------------------------------------------------------------------
 *                                       Literal Definitions
 *------------------------------------------------------------------------------------------------*/
-#define RIGHT_MOTOR_PIN					(23) 
-#define RIGHT_MOTOR_DIRECTION_PIN		(25)
-#define RIGHT_MOTOR_MICROSTEP_1_PIN		(27)
-#define RIGHT_MOTOR_MICROSTEP_2_PIN		(29)
-#define RIGHT_MOTOR_MICROSTEP_3_PIN		(31)
-#define LEFT_MOTOR_PIN					(33)
-#define LEFT_MOTOR_DIRECTION_PIN		(35)
-#define LEFT_MOTOR_MICROSTEP_1_PIN		(37)
-#define LEFT_MOTOR_MICROSTEP_2_PIN		(39)
-#define LEFT_MOTOR_MICROSTEP_3_PIN		(41)
+#define RIGHT_MOTOR_ENABLE_PIN                  (37)
+#define RIGHT_MOTOR_MICROSTEP_0_PIN		(35)
+#define RIGHT_MOTOR_MICROSTEP_1_PIN		(33)
+#define RIGHT_MOTOR_MICROSTEP_2_PIN		(31)
+#define RIGHT_MOTOR_RESET_PIN                   (29)
+#define RIGHT_MOTOR_SLEEP_PIN                   (27)
+#define RIGHT_MOTOR_PIN				(25) 
+#define RIGHT_MOTOR_DIRECTION_PIN		(23)
+
+#define LEFT_MOTOR_ENABLE_PIN                   (53)
+#define LEFT_MOTOR_MICROSTEP_0_PIN		(51)
+#define LEFT_MOTOR_MICROSTEP_1_PIN		(49)
+#define LEFT_MOTOR_MICROSTEP_2_PIN		(47)
+#define LEFT_MOTOR_RESET_PIN                    (45)
+#define LEFT_MOTOR_SLEEP_PIN                    (43)
+#define LEFT_MOTOR_PIN		        	(41)
+#define LEFT_MOTOR_DIRECTION_PIN		(39)
+
 #define IR_SENSOR_1_PIN					(A0)
 #define IR_SENSOR_2_PIN					(A1)
 #define IR_SENSOR_3_PIN					(A2)
 #define IR_SENSOR_4_PIN					(A3)
-#define IR_SENSOR_5_PIN					(A4)
-#define IR_SENSOR_6_PIN					(A5)
 #define LED_PIN_1					(19)
 #define LED_PIN_2					(21)
 #define ARM_SERVO_PIN					(2)
-#define IRIS_SERVO_PIN					(3)
+#define IRIS_SERVO_PIN					(4)
 
 /*-------------------------------------------------------------------------------------------------
 *                                             Types
